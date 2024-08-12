29e5aa9e11b3fcdca3dab2e6a1d92647c532fbb2
Santus22 <sarangomoran@santajuanalestonnac.edu.co>
2024-08-12 15:57:41 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`nivel2`)
+    scene.setBackgroundColor(0)
+})
+scene.setBackgroundColor(0)
+tiles.setCurrentTilemap(tilemap`nivel2`)
